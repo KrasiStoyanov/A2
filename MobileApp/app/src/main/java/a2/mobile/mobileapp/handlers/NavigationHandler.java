222ca00c1ca63eede487ebe0826e5910014010ae
Alreadydo <33254518+Alreadydo@users.noreply.github.com>
2020-05-25 12:49:22 +0200

    Update NavigationHandler.java

--- MobileApp/app/src/main/java/a2/mobile/mobileapp/handlers/NavigationHandler.java
+++ MobileApp/app/src/main/java/a2/mobile/mobileapp/handlers/NavigationHandler.java
@@ -2,6 +2,7 @@ package a2.mobile.mobileapp.handlers;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import com.mapbox.api.directions.v5.models.BannerInstructions;
 import com.mapbox.api.directions.v5.models.BannerText;
@@ -44,12 +45,14 @@ public class NavigationHandler {
 
     private static void updateIconName() {
         iconName = direction;
-        sendDataToBladeApp("icon name", iconName);
+        sendDataToBladeApp("icon_name", iconName);
+        Log.e("icon_name",iconName);
     }
 
     public static void updateDistanceRemaining(int distanceRemaining) {
         distance = distanceRemaining;
-        sendDataToBladeApp("distance remaining", String.valueOf(distance));
+        sendDataToBladeApp("distance_remaining", String.valueOf(distance));
+        Log.e("distance",String.valueOf(distance));
     }
 
     public static void updateInterestPoint(PointOfInterest interestPoint) {
@@ -58,13 +61,17 @@ public class NavigationHandler {
         interestPointPriority = interestPoint.getPriority();
 
         sendDataToBladeApp("interest point title", interestPointTitle);
+        Log.e("title",interestPointTitle);
         sendDataToBladeApp("interest point description", interestPointDescription);
+        Log.e("description",interestPointDescription);
         sendDataToBladeApp("interest point priority", interestPointPriority.toString());
+        Log.e("priority",interestPointPriority.toString());
     }
 
     private static void sendDataToBladeApp(String name, String data) {
         Intent sendIntent = new Intent(NavigationConstants.SEND_DATA_ACTION);
         sendIntent.setPackage("com.example.bladeapp");
+        //sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         sendIntent.putExtra(name, data);
 
         Connectivity.get(mContext).sendBroadcast(sendIntent);
